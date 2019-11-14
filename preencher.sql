1a0f4e3bd99bbc75cbf37b94241137afe25b6ee1
DarlanGabriel <darlan.acari@gmail.com>
2019-11-14 16:52:00 -0300

    Preenchendo o banco

---
+++ preencher.sql
@@ -0,0 +1,5 @@
+insert into dependente (coddepend, matricula, nome, sexo) Values (0, 123, "Xuxa", "F");
+
+insert into empregado (matricula, nome, endereco, salario, supervisor, depto) Values (0, "Joao", "Rua dos doidos", 0.0, NULL, NULL);
+
+insert into departamento (coddep, nome, gerente, dataini) Values (0, "SEILA", NULL, NULL);
