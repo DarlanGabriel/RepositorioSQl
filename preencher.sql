33c2dd3b6e7022feed90b01a36f9fe59baa1ac63
DarlanGabriel <darlan.acari@gmail.com>
2019-11-14 17:09:21 -0300

    Adicionando os dados parte 1

--- preencher.sql
+++ preencher.sql
@@ -3,3 +3,7 @@ insert into dependente (coddepend, matricula, nome, sexo) Values (0, 123, "Xuxa"
 insert into empregado (matricula, nome, endereco, salario, supervisor, depto) Values (0, "Joao", "Rua dos doidos", 0.0, NULL, NULL);
 
 insert into departamento (coddep, nome, gerente, dataini) Values (0, "SEILA", NULL, NULL);
+
+insert into projeto(codproj, nome, localidade, depart) values (0, "Projetim", "Um canto ai", NULL);
+
+insert into alocacao(matric, codigop, horas) values (0, 0, NULL);
